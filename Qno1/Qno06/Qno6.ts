e6cbf2c8735ae3ae2ca85d5ab327992c9f7c1f66
Fatima Nazeer <147838099+FatimaNazeer777@users.noreply.github.com>
2024-02-17 20:22:29 +0500

    45 assignments
    45 assignments of typescript.

---
+++ Qno1/Qno06/Qno6.ts
@@ -0,0 +1,5 @@
+/*Q6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.*/
+
+console.log("mynameisFatima");
+console.log("my\tname\tis\tFatima");  //used for spaces b/w words.
+console.log("my\nname\nis\nFatima"); //used for next lines.
